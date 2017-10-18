cf6964999cebd82ff53c8e0a5ea013ac712c54de
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2017-10-18 13:21:05 +0200

    Read buffersize from file if fortran raw

--- corsikaio/io.py
+++ corsikaio/io.py
@@ -1,3 +1,5 @@
+import struct
+
 from .subblocks import (
     parse_run_header,
     parse_event_header
@@ -6,39 +8,60 @@ from .subblocks import (
 from .constants import BLOCK_SIZE_BYTES
 
 
-def read_block(f, fortran_raw=False):
-    if fortran_raw is True:
-        pos = f.tell() - 4
+def read_buffer_size(f):
+    '''
+    Reads the first 4 bytes of a file and checks if
+    it is the 'RUNH' designation None is returned,
+    if not interpret it as unsigned integer, the
+    size of the CORSIKA buffer in bytes
+    '''
+    pos = f.tell()
 
-        if pos % 22932 == 0:
+    f.seek(0)
+    data = f.read(4)
+    f.seek(pos)
+
+    if data == b'RUNH':
+        return None
+
+    buffer_size, = struct.unpack('I', data)
+
+    return buffer_size
+
+
+def read_block(f, buffer_size=None):
+    '''
+    Reads a block of CORSIKA output, e.g. 273 4-byte floats.
+
+    Under some conditions, CORSIKA writes output as FORTRAN
+    raw file format. This means, there is a 4-byte unsigned
+    integer at each
+    '''
+    pos = f.tell()
+
+    if buffer_size is not None:
+        if pos == 0:
+            f.seek(4)
+
+        pos -= 4
+
+        if pos % buffer_size == 0:
             f.read(8)
 
     return f.read(BLOCK_SIZE_BYTES)
 
 
-def is_fortran_raw(f):
-    pos = f.tell()
-    f.seek(0)
-    fortran_raw = f.read(4) != b'RUNH'
-    f.seek(pos)
-    return fortran_raw
-
-
 def read_corsika_headers(inputfile):
     inputfile.seek(0)
 
-    fortran_raw = is_fortran_raw(inputfile)
-
-    if fortran_raw:
-        inputfile.seek(4)
-
-    byte_data = read_block(BLOCK_SIZE_BYTES)
+    buffer_size = read_buffer_size(inputfile)
+    byte_data = read_block(inputfile, buffer_size)
     run_header = parse_run_header(byte_data)
 
     # Read Eventheader
     event_header_data = b''
     while True:
-        byte_data = read_block(inputfile, fortran_raw)
+        byte_data = read_block(inputfile, buffer_size)
 
         # No more Shower in file
         if byte_data[0:4] == b'RUNE':
--- tests/test_io.py
+++ tests/test_io.py
@@ -1,15 +1,25 @@
+test_files = (
+    'tests/resources/mmcs65',
+    'tests/resources/corsika75600'
+)
+
+
 def test_fortran_raw():
-    from corsikaio import is_fortran_raw
+    from corsikaio import read_buffer_size
 
     with open('tests/resources/mmcs65', 'rb') as f:
-        assert not is_fortran_raw(f)
+        assert read_buffer_size(f) is None
+
+    with open('tests/resources/corsika75600', 'rb') as f:
+        assert read_buffer_size(f) == 22932  # standard CORSIKA buffer size
 
 
 def test_read_block():
-    from corsikaio import is_fortran_raw, read_block
+    from corsikaio import read_buffer_size, read_block
 
-    with open('tests/resources/mmcs65', 'rb') as f:
+    for path in test_files:
+        with open(path, 'rb') as f:
+            buffer_size = read_buffer_size(f)
+            block = read_block(f, buffer_size)
+            assert block[:4] == b'RUNH'
 
-        fortran_raw = is_fortran_raw(f)
-        block = read_block(f, fortran_raw)
-        assert block[:4] == b'RUNH'
