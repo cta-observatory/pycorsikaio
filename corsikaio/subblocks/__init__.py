0623792aa2bd504444350bc7bbf60199c2b5e249
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2017-10-18 13:23:29 +0200

    Improve version handling

--- corsikaio/subblocks/__init__.py
+++ corsikaio/subblocks/__init__.py
@@ -3,6 +3,7 @@ import struct
 
 from .run_header import run_header_dtype_65, run_header_dtype_75
 from .event_header import event_header_dtype_65, event_header_dtype_75
+from ..constants import RUNH_VERSION_POSITION, EVTH_VERSION_POSITION
 
 __all__ = [
     'parse_event_header',
@@ -21,15 +22,17 @@ event_header_types = {
 
 
 def parse_run_header(run_header_bytes):
-    version = read_version(run_header_bytes, 4)
+    version = get_version(run_header_bytes, RUNH_VERSION_POSITION)
+    version = round(version, 1)
     return np.frombuffer(run_header_bytes, dtype=run_header_types[version])
 
 
 def parse_event_header(event_header_bytes):
-    version = read_version(event_header_bytes, 46)
+    version = get_version(event_header_bytes, EVTH_VERSION_POSITION)
+    version = round(version, 1)
     return np.frombuffer(event_header_bytes, dtype=event_header_types[version])
 
 
-def read_version(header_bytes, version_pos):
+def get_version(header_bytes, version_pos):
     sl = slice(4 * (version_pos - 1), 4 * version_pos)
-    return struct.unpack('f', header_bytes[sl])[0]
+    return round(struct.unpack('f', header_bytes[sl])[0], 2)
