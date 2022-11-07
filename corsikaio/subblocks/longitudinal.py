d3e5f396dd4ecd3a7414d2d40e72d6177920cf32
Michele Peresano <michele.peresano@cea.fr>
2022-11-07 19:36:09 +0100

    Update longitudinal fields

--- corsikaio/subblocks/longitudinal.py
+++ corsikaio/subblocks/longitudinal.py
@@ -1,30 +1,34 @@
-import numpy as np
+from .dtypes import Field, build_data_dtype
 
-longitudinal_header_dtype = np.dtype([
-    ('LONG', 'S4'),
-    ('event_number', 'f4'),
-    ('particle_id', 'f4'),
-    ('total_energy', 'f4'),
-    ('n_longitudinal', 'f4'),
-    ('longitudinal_id', 'f4'),
-    ('first_interaction_height', 'f4'),
-    ('zenith', 'f4'),
-    ('azimuth', 'f4'),
-    ('energy_cutoff_hadrons', 'f4'),
-    ('energy_cutoff_muons', 'f4'),
-    ('energy_cutoff_electrons', 'f4'),
-    ('energy_cutoff_photons', 'f4'),
-])
 
-longitudinal_data_dtype = np.dtype([
-    ('vertical_depth', 'f4'),
-    ('n_photons', 'f4'),
-    ('n_e_plus', 'f4'),
-    ('n_e_minus', 'f4'),
-    ('n_mu_plus', 'f4'),
-    ('n_mu_minus', 'f4'),
-    ('n_hadrons', 'f4'),
-    ('n_charged', 'f4'),
-    ('n_nuclei', 'f4'),
-    ('n_cherenkov', 'f4'),
-])
+longitudinal_header_fields = [
+    Field(1, "LONG", shape="S4"),
+    Field(2, "event_number", shape="f4"),
+    Field(3, "particle_id", shape="f4"),
+    Field(4, "total_energy", unit="GeV", shape="f4"),
+    Field(5, "n_longitudinal", shape="f4"),
+    Field(6, "longitudinal_id", shape="f4"),
+    Field(7, "first_interaction_height", unit="g/cm2", shape="f4"),
+    Field(8, "zenith", unit="rad", shape="f4"),
+    Field(9, "azimuth", unit="rad", shape="f4"),
+    Field(10, "energy_cutoff_hadrons", unit="GeV", shape="f4"),
+    Field(11, "energy_cutoff_muons", unit="GeV", shape="f4"),
+    Field(12, "energy_cutoff_electrons", unit="GeV", shape="f4"),
+    Field(13, "energy_cutoff_photons", unit="GeV", shape="f4"),
+]
+
+longitudinal_data_fields = [
+    Field(1, "vertical_depth", unit="g/cm2", shape="f4"),
+    Field(2, "n_photons", shape="f4"),
+    Field(3, "n_e_plus", shape="f4"),
+    Field(4, "n_e_minus", shape="f4"),
+    Field(5, "n_mu_plus", shape="f4"),
+    Field(6, "n_mu_minus", shape="f4"),
+    Field(7, "n_hadrons", shape="f4"),
+    Field(8, "n_charged", shape="f4"),
+    Field(9, "n_nuclei", shape="f4"),
+    Field(10, "n_cherenkov", shape="f4"),
+]
+
+longitudinal_header_dtype = build_data_dtype(longitudinal_header_fields)
+longitudinal_data_dtype = build_data_dtype(longitudinal_data_fields)
