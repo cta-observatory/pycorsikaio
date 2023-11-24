1d2863b969af2b0230e787d170b6d381f3b9a3ee
Maximilian Linhoff <maximilian.linhoff@tu-dortmund.de>
2023-11-24 17:48:17 +0100

    Test fit function by comparing to distribution

--- tests/test_longitudinal.py
+++ tests/test_longitudinal.py
@@ -3,6 +3,7 @@ import numpy as np
 
 def test_longitudinal():
     from corsikaio import read_longitudinal_distributions
+    from corsikaio import longitudinal_fit_function
     path = "./tests/resources/corsika_77500_particle.long"
 
 
@@ -14,6 +15,15 @@ def test_longitudinal():
         assert len(longi["energy_deposition"]) == 105
         np.testing.assert_array_equal(np.diff(longi["particles"]["depth"]), 10)
         np.testing.assert_array_equal(np.diff(longi["energy_deposition"]["depth"]), 10)
+
+        particles = longi["particles"]
+
+
+        # can only meaningfully compare hight statistics area
+        mask = particles["charged"] > 500
+        fit = longitudinal_fit_function(particles["depth"][mask], *longi["parameters"])
+        np.testing.assert_allclose(particles["charged"][mask], fit, rtol=0.2)
+
 
     assert n_showers == 5
 
