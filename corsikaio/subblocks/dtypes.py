335b7dd6fa2fb453a51bea4741f8ec09f7d17ca7
Gernot Maier <gernot.maier@desy.de>
2025-12-08 11:59:47 +0100

    namedtuple defaults

--- corsikaio/subblocks/dtypes.py
+++ corsikaio/subblocks/dtypes.py
@@ -3,10 +3,11 @@ import warnings
 import numpy as np
 
 
-Field = namedtuple("Field", ["position", "name", "unit", "shape", "dtype", "min_version"])
-
-# set defaults for unit, shape, dtype and min_version; works for the 4 right most attributes
-Field.__new__.__defaults__ = (None, 1, "f4", 0)
+Field = namedtuple(
+    "Field",
+    ["position", "name", "unit", "shape", "dtype", "min_version"],
+    defaults=(None, 1, "f4", 0),
+)
 
 
 def build_dtype(fields, itemsize=4 * 273):
